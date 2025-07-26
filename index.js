c9ae18a63485b7e00314a58ebf20631ffad857b5
Fayez Morsel <fayezmorsel77@gmail.com>
2025-07-26 23:01:08 +0300

    aside boolean

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard
+let hasDiscountCode = true
 
-// Write the conditional according to these rules:
-
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
-// else -> "You're out of the game! 😭"
-
-
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-} else {
-    console.log("You're out of the game! 😭")
+function processOrder() {
+    if (hasDiscountCode) {
+        console.log('Discount applied to food order')
+        hasDiscountCode = false
+    } else {
+        console.log('No discount applied')
+    }
 }
+
+processOrder()
+processOrder()
